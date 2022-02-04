15846bce69f6106b2251e16367903ece129da6f1
Antonis <marasantonis@gmail.com>
2022-02-04 21:40:15 +0200

    number values wip

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
 const displayContent = document.querySelector(".display-text");
+const allButtons = document.querySelectorAll("button");
 const numberButtons = document.querySelectorAll(".numbers");
 const utilitiesButtons = document.querySelectorAll(".utilities");
 const operatorsButtons = document.querySelectorAll(".operators");
-let numberArr = [];
+let numberOneArr;
+let numberTwoArr;
+displayContent.textContent = "";
 
-const testFunction = (e) => {
-  numberArr.push(e.target.value);
-  displayContent.textContent = numberArr.join("");
-  if (numberArr.join("").length > 13) {
-    displayContent.textContent = numberArr.slice(0, 13).join("");
+// const getNumberButtonValue = (e) => {
+//   numberArr.push(e.target.value);
+//   displayContent.textContent = numberArr.join("");
+//   if (numberArr.join("").length > 13) {
+//     displayContent.textContent = numberArr.slice(0, 13).join("");
+//   }
+// };
+
+// numberButtons.forEach((button) => {
+//   button.addEventListener("click", getNumberButtonValue);
+// });
+
+const clearButton = (e) => {
+  if (e.target.value === "C") {
+    displayContent.textContent = "0";
   }
-  console.log(displayContent.textContent);
-
-  console.log(displayContent.textContent.length);
-  console.log(numberArr.join("").length);
 };
 
-numberButtons.forEach((button) => {
-  button.addEventListener("click", testFunction);
+const numberButtonValues = (e) => {
+  displayContent.textContent += e.target.value;
+};
+
+allButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    e.target.value;
+    numberButtonValues(e);
+    clearButton(e);
+  });
 });
