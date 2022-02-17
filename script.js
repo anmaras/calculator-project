5aada8ee85ebd6611a7cb4268ce7d1af768d627f
Antonis <marasantonis@gmail.com>
2022-02-17 20:48:32 +0200

    delete last and clear all functions added

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ const display = document.querySelector(".display-text");
 const allButtons = document.querySelectorAll("button");
 const numberButtons = document.querySelectorAll(".numbers");
 const decimalButton = document.querySelector(".decimal");
+const clearButton = document.querySelector(".clear");
+const deleteButton = document.querySelector(".delete");
 console.log(decimalButton);
 
-decimalButton.addEventListener("click", () => appendDecimal());
+decimalButton.addEventListener("click", appendDecimal);
+clearButton.addEventListener("click", clearAll);
+deleteButton.addEventListener("click", deleteLast);
 
 numberButtons.forEach((button) => {
   button.addEventListener("click", () => appendNumber(button.textContent));
@@ -17,4 +21,13 @@ function appendNumber(number) {
 
 function appendDecimal() {
   if (!display.textContent.includes(".")) display.textContent += ".";
+}
+
+function clearAll() {
+  display.textContent = "0";
+}
+
+function deleteLast() {
+  display.textContent = display.textContent.slice(0, -1);
+  if (display.textContent === "") display.textContent = "0";
 }
