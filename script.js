bf18fc24db66df293781de741721e67c2833c77b
Antonis <marasantonis@gmail.com>
2022-02-21 11:21:39 +0200

    operator function added

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const numberButtons = document.querySelectorAll(".numbers");
 const decimalButton = document.querySelector(".decimal");
 const clearButton = document.querySelector(".clear");
 const deleteButton = document.querySelector(".delete");
+const operatorButtons = document.querySelectorAll(".operators");
+const PLUS_OPERATOR = "+";
+const MULTIPLY_OPERATOR = "*";
+const DIVIDE_OPERATOR = "/";
+const SUBTRACT_OPERATOR = "-";
 
 decimalButton.addEventListener("click", appendDecimal);
 clearButton.addEventListener("click", clearAll);
@@ -10,6 +15,9 @@ deleteButton.addEventListener("click", deleteLast);
 
 numberButtons.forEach((button) => {
   button.addEventListener("click", () => appendNumber(button.textContent));
+});
+operatorButtons.forEach((button) => {
+  button.addEventListener("click", () => operator(button.textContent));
 });
 
 function appendNumber(number) {
@@ -30,4 +38,16 @@ function clearAll() {
 function deleteLast() {
   display.textContent = display.textContent.slice(0, -1);
   if (display.textContent === "") display.textContent = "0";
+}
+
+function operator(operator) {
+  let buttonText = operator;
+  let operatorSelection;
+  if (
+    buttonText === PLUS_OPERATOR ||
+    buttonText === MULTIPLY_OPERATOR ||
+    buttonText === DIVIDE_OPERATOR ||
+    buttonText === SUBTRACT_OPERATOR
+  )
+    operatorSelection = buttonText;
 }
