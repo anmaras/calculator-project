43595f8db89ad4ae3e291dab7afa7263d7067b3f
Antonis <marasantonis@gmail.com>
2022-02-21 11:08:15 +0200

    max length condition set for append

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const display = document.querySelector(".display-text");
-const allButtons = document.querySelectorAll("button");
 const numberButtons = document.querySelectorAll(".numbers");
 const decimalButton = document.querySelector(".decimal");
 const clearButton = document.querySelector(".clear");
@@ -14,12 +13,14 @@ numberButtons.forEach((button) => {
 });
 
 function appendNumber(number) {
-  if (display.textContent === "0") display.textContent = number;
-  else display.textContent += number;
+  if (display.textContent.length !== 13)
+    if (display.textContent === "0") display.textContent = number;
+    else display.textContent += number;
 }
 
 function appendDecimal() {
-  if (!display.textContent.includes(".")) display.textContent += ".";
+  if (display.textContent.length !== 13)
+    if (!display.textContent.includes(".")) display.textContent += ".";
 }
 
 function clearAll() {
